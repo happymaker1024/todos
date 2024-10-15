79336c6ac0765031499dfcb5923da15b6f6da59f
kimkha <happymaker1024@gmail.com>
2024-10-15 15:03:32 +0900

    todo DB 테이블 생성 및 저장하기

--- database.py
+++ database.py
@@ -2,10 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+# sqlite3 엔진을 정의, DB파일 todo.sqlite3
 DB_URL = 'sqlite:///todo.sqlite3'
 
+# 엔진객체 생성
 engine = create_engine(DB_URL, connect_args={'check_same_thread': False})
 
+# DB 연결(세션) 객체 생성
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-from fastapi import FastAPI, Form, Request
+from fastapi import FastAPI, Form, Request, Depends
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
 import uvicorn
-from database import Base, SessionLocal
+from database import Base, SessionLocal, engine
 import models
-from database import Base, engine
 
 app = FastAPI()
 
 # DB 엔진 연결
+# -> models.py에 정의한 클래스를 통해 db에 테이블 생성
 Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    except :
+        print("db연결 오류")
+    finally:
+        db.close()
 
 # html 문서를 위한 객체
 templates = Jinja2Templates(directory="templates")
@@ -30,10 +40,22 @@ async def home(request: Request):
         )
 
 @app.post("/add")
-async def add(request: Request, task: str = Form(...)):
-    # 클라이언트에서 textarea에서 입력 데이터 넘어오면
-    # db 테이블에 저장하고 결과를 html에 랜더링에서 리턴
+async def add(request: Request, task: str = Form(...), 
+              db_ss: Session = Depends(get_db)):
+    # 클라이언트에서 textarea에서 입력 데이터 넘어온것 확인
     print(task)
+    # 클라이언트에서 넘어온 task를 Todo 객체로 생성
+    todo = models.Todo(task=task)
+    # 의존성 주입에서 처리함 Depends(get_db) : 엔진객체생성, 세션연결
+    # db 테이블에 task 저장하기
+    db_ss.add(todo)
+    # db에 실제 저장, commit
+    db_ss.commit()
+    return task
+
+    
+    # 결과를 html에 랜더링에서 리턴
+
     pass
 
 # python main.py
